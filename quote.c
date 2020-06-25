8045c0105efdcb3b9acd4b786c48142d111e0230
hpark <hpark@DESKTOP-UTQPNKR.localdomain>
2020-06-25 17:02:26 +0900

    hpark

---
+++ quote.c
@@ -0,0 +1,59 @@
+#include "minishell.h"
+
+int ft_quote_cnt(char *line, int flag)
+{
+	int cnt_dq;
+	int cnt_q;
+	cnt_dq = 0;
+	cnt_q = 0;
+
+	if (flag == 1)
+		cnt_dq = 1;
+	if (flag == 0)
+		cnt_q = 1;
+	while (*line)
+	{
+		if (cnt_q % 2 == 0 && *line == '"')
+			cnt_dq++;
+		if (cnt_dq % 2 == 0 && *line == '\'')
+			cnt_q++;
+		line++;
+	}
+	if (cnt_dq % 2 == 1)
+		return (2);
+	if (cnt_q % 2 == 1)
+		return (1);
+	return (0);
+}
+
+int	ft_ret(char *str, int nb)
+{
+	ft_puts(str);
+	return (nb);
+}
+
+char	*ft_quote(char *line)
+{
+	int		flag;
+	char	*tmp;
+
+	tmp = line;
+	while (*line != '"' && *line != '\'')
+		line++;
+	if (ft_quote_cnt(line, 2) == 2)
+		flag = ft_ret("dquote> ", 1);
+	else
+		flag = ft_ret("quote> ", 0);
+	while (get_next_line(1, &line) > 0)
+	{
+		tmp = ft_strjoin_newline(tmp, line);
+		if (*line != '\0')
+		{
+			if ((flag = (ft_quote_cnt(line, flag))) == 0)
+				return (tmp);
+			flag = flag - 1;
+		}
+		(flag == 1) ? ft_puts("dquote> ") : ft_puts("quote> ");
+	}
+	return (tmp);
+}
---
+++ quote_cmd.c
@@ -0,0 +1,87 @@
+#include "minishell.h"
+
+int	end_of_quote(char *tmp)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (*(tmp - 1) == '"')
+	{
+		while (*tmp++ != '"')
+			cnt++;
+	}
+	else if (*(tmp - 1) == '\'')
+	{
+		while (*tmp++ != '\'')
+			cnt++;
+	}
+	return (cnt);
+}
+
+int	cnt_info(char *line)
+{
+	char	*tmp;
+	int		cnt;
+
+	tmp = line;
+	cnt = 0;
+	while (*tmp++)
+	{
+		if (*tmp == '"' || *tmp == '\'' || *tmp == ' ')
+			cnt++;
+	}
+	return (cnt);
+}
+
+int		until_quote(char *tmp)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (*(tmp + cnt))
+	{
+		if (*(tmp + cnt) != '"' && *(tmp + cnt) != '\'' && *(tmp + cnt) != ' ')
+			cnt++;
+		else
+			break;
+	}
+	return (cnt);
+}
+
+char	**get_info(char *tmp, char	**info)
+{
+	int		cnt;
+	int		i;
+
+	i = 0;
+	while (*tmp)
+	{
+		while (*tmp && (*tmp != '"' && *tmp != '\''))
+		{
+			while (*tmp && *tmp == ' ')
+				tmp++;
+			cnt = until_quote(tmp);
+			if (cnt > 0)
+				info[i++] = ft_substr(tmp, 0, cnt);
+			tmp += cnt;
+		}
+		if (*tmp++)
+		{
+			cnt = end_of_quote(tmp);
+			info[i++] = ft_substr(tmp, 0, cnt);
+			tmp += cnt + 1;
+		}
+	}
+	info[i] = 0;
+	return (info);
+}
+
+int	ft_quote_cmd(char *line)
+{
+	char	**info;
+
+	info = malloc(sizeof(char *) * (cnt_info(line) + 1));
+	info = get_info(line, info);
+	read_cmd(info, 0, 0, g_env);
+	return (0);
+}
