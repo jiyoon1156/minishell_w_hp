29162e20c178bbc275797ca9fb56d8040e1a847e
Hyunyoung Park <hpark@c2r7s7.42seoul.kr>
2020-06-29 15:48:05 +0900

    fd in fd and header

--- new_mini/minishell.c
+++ new_mini/minishell.c
@@ -1,4 +1,4 @@
-#include "minishell.h"
+  #include "minishell.h"
 
 int	g_ret = 0;
 char **g_env = 0;
@@ -37,94 +37,36 @@ char	**ft_cpenv(char **envp)
 	return (env);
 }
 
-int	quote_check(int *quote, char c)
-{
-	if (*quote == 0 && (c == '\'' || c == '"'))
-		*quote = c;
-	else if (*quote != 0 && *quote == c)
-		*quote = 0;
-	else
-		return (0);
-	return (1);
-}
-
-int		count_char(char *line, int c)
-{
-	int		i;
-	int		cnt;
-
-	i = 0;
-	cnt = 0;
-	while (line[i])
-	{
-		if (line[i] == c)
-			cnt++;
-		i++;
-	}
-	return (cnt);
-}
-
-/* ;를 기준으로 commands 끊기
-		따옴표 고려해서 */
-
-char	**get_cmds(char *line, char sep)
-{
-	char	**cmds;
-	char	*tmp;
-	int		quote;
-	int		i;
-
-	cmds = malloc(sizeof(char *) * (count_char(line, sep) + 2));
-	i = 0;
-	quote = 0;
-	tmp = line;
-	while (*line)
-	{
-		quote_check(&quote, *line);
-		if (*line == sep && quote == 0)
-		{
-			cmds[i++] = ft_substr(tmp, 0, (line++ - tmp));
-			while (*line && *line == ' ')
-				line++;
-			if (*(tmp = line) == sep)//; 가 두개일때 파스에러
-			{
-				//에러 처리하기 - free하고
-				ft_puts("parse error\n");
-				return (0);
-			}
-		}
-		line++;
-	}
-	cmds[i++] = ft_substr(tmp, 0, (line - tmp));
-	cmds[i] = 0;
-	return (cmds);
-}
-
-int		execute_cmds(char *cmds)
+int		execute_cmd(char *line)
 {
 	char	**cmd;
-	char	**info;
-	int		i;
+	int		idx;
 	int		j;
 
-	if (!(cmd = get_cmds(cmds, '|')))
-		return (0);
-	i = 0;
-	while (cmd[i])
+	if (ft_strchr(line, '"') || ft_strchr(line, '\''))
 	{
-
-		info = get_info(cmd[i++]);
-		//read_cmd(info);	/* redirection, pipe 처리도 같이 */
-		//ft_cmd(&cmds[idx++][j]);
+		if ((ft_quote_cnt(line, -1)) != 0)
+			line = ft_quote(line);
+		ft_quote_cmd(line);
 	}
-	ft_cmd(info);
+	else
+	{
+		cmd = ft_split(line, ';');
+		idx = 0;
+		while (cmd[idx])
+		{
+			j = 0;
+			while (cmd[idx][j] == ' ')
+				j++;
+			ft_cmd(&cmd[idx++][j]);
+		}
+	}
 	return (0);
 }
 
 int		main(int ac, char **av, char **envp)
 {
 	char	*line;
-	char	**cmds;
 
 	(void)ac;
 	(void)av;
@@ -136,13 +78,7 @@ int		main(int ac, char **av, char **envp)
 	{
 		if (*line != '\0')
 		{
-			if ((ft_quote_cnt(line, -1)) != 0)
-				line = ft_quote(line);
-			if ((cmds = get_cmds(line, ';')))
-			{
-				while (*cmds)
-					execute_cmds(*cmds++);
-			}
+			execute_cmd(line);
 			free(line);
 			line = 0;
 		}
--- new_mini/minishell.h
+++ new_mini/minishell.h
@@ -12,25 +12,24 @@ extern char **g_env;
 char	**get_info(char *line);
 int		count_char(char *line, int c);
 int		quote_check(int *quote, char c);
-
 int		redirection(char *redir, int r_flag);
-int		ft_echo(char **info, char **redir, int r_flag);
-int		ft_pwd(char **info, char **redir, int r_flag);
+int		ft_echo(char **info, int fd);
+int		ft_pwd(char **info, int fd);
 int		ft_cd(char **info);
 int		ft_unset(char **info, char **envp);
-int		ft_env(char **info, char **envp, char **redir, int r_flag);
+int		ft_env(char **info, char **envp, int fd);
 int		ft_env_valid(char **info);
-int		ft_print_env(char **envp, char **redir, int r_flag);
+int		ft_print_env(char **envp, int fd);
 int		ft_print_env_1(char **info);
 void	env_add(char *info, char **env);
-int		ft_export(char **info, char **envp, char **redir, int r_flag);
-void	read_cmd(char **info, char **redir, int r_flag, char **envp);
+int		ft_export(char **info, char **envp, int fd);
+void	read_cmd(char **info, int fd);
 int		ft_quote_cnt(char *line, int flag);
 char	*ft_quote(char *line);
 int		ft_quote_cmd(char *line);
 int		ft_cmd(char **infos);
 void	fork_process(int *pipefd, char **cmd, char **path, int i);
 void	ft_pipe(char *cm);
-void	read_env(char **info, char **redir, int r_flag);
+void	read_env(char **info, int fd);
 
 #endif
