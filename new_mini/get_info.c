5a1ad498077690f444fa254f8ec1cf754eeefda2
Hyunyoung Park <hpark@c1r6s7.42seoul.kr>
2020-07-23 11:19:26 +0900

    norm fixing

--- new_mini/get_info.c
+++ new_mini/get_info.c
@@ -1,9 +1,7 @@
 #include "minishell.h"
 
-//quote 없애고 넣어주는 함수
 char	*ft_substr_without_quote(char *str, unsigned int start, int len)
 {
-	//환경변수($처리도 여기서?)
 	char	*ptr;
 	int		i;
 	int		quote;
@@ -35,7 +33,6 @@ char	*info_redirection(char *line)
 	i = 0;
 	if (!(info = malloc(sizeof(char) * 2)))
 		return (0);
-	//QUOTE 밖에 있는 redirection 은 작동해야하기에 판별하는걸 -1 곱해주는것으로 한다.
 	info[i] = *(line++) * (-1);
 	info[i + 1] = '\0';
 	return (info);
@@ -48,15 +45,12 @@ void	get_info2(char *line, char **info, char *tmp, int quote)
 		quote_check(&quote, *line);
 		if (quote == 0 && ft_strchr(" ><", *line))
 		{
-			if (line - tmp > 0)//둘이 겹치면 info에 넣어주면 안되니까
+			if (line - tmp > 0)
 				*info++ = ft_substr_without_quote(tmp, 0, (line - tmp));
 			while (*line && *line == ' ')
 				line++;
-			//>> 일때랑 여러개일 때 파스에러처리
-			if (*line == '>' || *line == '<') //quote 0 조건이 필요한가??
+			if (*line == '>' || *line == '<')
 				*info++ = info_redirection(line++);
-			// else if (*line == '|' && quote == 0)
-			// 	*info++ = info_pipe_or_redirection(line++);
 			while (*line && *line == ' ')
 				line++;
 			tmp = line;
@@ -68,8 +62,6 @@ void	get_info2(char *line, char **info, char *tmp, int quote)
 		*info++ = ft_substr_without_quote(tmp, 0, (line - tmp));
 	*info = 0;
 }
-/* cmd 에서 info 가져오기
-		따옴표 고려해서 */
 
 char	**get_info(char *line)
 {
@@ -84,15 +76,5 @@ char	**get_info(char *line)
 		line++;
 	tmp = line;
 	get_info2(line, info, tmp, quote);
-
-///////info 확인코드///////
-	// int i = 0;
-	// while (info[i])
-	// {
-	// 	ft_puts(info[i++]);
-	// 	ft_puts("!\n");
-	// }
-//////////////////////////
-	
 	return (info);
 }
--- new_mini/redirection.c
+++ new_mini/redirection.c
@@ -10,9 +10,7 @@ int	redirection(char *redir, int r_flag)
 		if (r_flag == 1)
 			fd = open(redir, O_APPEND | O_CREAT | O_WRONLY, S_IRWXU);
 		else if (r_flag == 0)
-			fd = open(redir, O_TRUNC | O_CREAT | O_WRONLY, S_IWRXU);
-		// else
-		// 	fd = open(redir, O_TRUNC | O_RDONLY, S_IRWXU);
+			fd = open(redir, O_TRUNC | O_CREAT | O_WRONLY, S_IRWXU);
 	}
 	if (fd == -1)
 		ft_putstr_fd("no such file or directory", 2);
